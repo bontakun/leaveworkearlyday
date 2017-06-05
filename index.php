49335d183c6122ad67a3bb4490d2451e13616b1a
Benjamin Dow <ben@bonta-kun.net>
2017-06-04 20:59:24 -0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>International Leave Work Early Day</title>
+		
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<meta name="author" content="bontakun" />
+		<meta name="description" content="International leave work early day, this holiday especially needs to celebrated by Strawberry eating Bolda's." />
+		<meta name="reason" content="This is all based on this article: https://www.sdworx.com/en-us/blog/2017-05-29-national-leave-the-office-early-day" />
+	</head>
+	<body>
+		<div id="pageWrapper">
+			<div id="pageBody">
+				<h1>International Leave Work Early Day</h1>
+			
+				<hr />
+			
+				<p>International leave work early day is <?php echo(date("F jS (l)", strtotime("friday"))) ?>, you've probably worked hard and long hours, celebrate it by leaving work early and start enjoying your weekend!</p>
+				
+				<p>An employee productivity specialist came up with the idea to give something back to employees who frequently work more than the standard 40 hours a week (see Bolda). It's celebrated every year and you're supposed to finish your work for the day and leave after you get approval from your boss figure (show this website to them if they don't believe you).</p>
+				
+				<p>
+					<a href="https://twitter.com/share?text=I%27m%20leaving%20work%20early%20today%2C%20here%27s%20why!%20%23LeaveTheOfficeEarlyDay%20%23NoRush&url=http://leaveworkearlyday.com">
+						<img alt="twitter logo" src="/Twitter_Logo_Blue.png" id="twitterImage" />Tweet and share.</a>
+				</p>
+			</div>
+			
+			<div id="pageFooter">
+				This page is brought to you by the good people of <a href="http://bontakun.net/">bontakun.net</a>, background provided by <a href="https://unsplash.com">Unsplash</a>.
+			</div>
+		</div>
+	</body>
+</html>
